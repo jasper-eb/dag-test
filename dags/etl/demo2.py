bc12d6f04063928761d7afbec2ef5891eccdf8eb
jasper-eb <jasper@eventbrite.com>
2020-04-24 14:39:24 -0500

    Second demo

---
+++ dags/etl/demo2.py
@@ -0,0 +1,46 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+from datetime import datetime, timedelta
+
+from library.etl.presto import PrestoETLClient
+
+default_args = {
+    "owner": "jasper",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 4, 23, 10),
+    "retries": 0,
+    "retry_delay": timedelta(minutes=2),
+    'schedule_interval': '@daily',
+}
+
+etl_1_kwargs = {
+    "queries_dict": [
+        {
+            "query": "resources/jasper/demo/drop.sql"
+        },
+        {
+            "query": "resources/jasper/demo/ctas",
+            "parameters": {
+                "date": datetime.today() - timedelta(days=1)
+            }
+        }
+    ]
+}
+
+etl_2_kwargs = {
+    "query": "../../../resources/jasper/demo/query.sql"
+}
+
+dag = DAG(
+    "python-etl",
+    description="Demo of presto ETL run through python",
+    default_args=default_args,
+)
+
+presto_etl = PrestoETLClient(etl_1_kwargs, default_args.get('owner'), 'presto-1.prod.dataf.eb')
+etl_1 = PythonOperator(
+    task_id='SomePrestoETL',
+    python_callable=presto_etl.run(),
+    dag=dag
+)
---
+++ library/etl/presto.py
@@ -0,0 +1,74 @@
+import presto
+
+from jinja2 import Template
+
+
+class PrestoClient(object):
+    def __init__(self, username, host, password=None, port=8080, https=False):
+        self.username = username
+        self.password = password
+        self.host = host
+        self.port = port
+        self.https = https
+        self.conn = None
+
+    def _get_http_connection(self):
+        conn = presto.dbapi.connect(
+            host=self.host,
+            port=self.port,
+            user=self.username,
+        )
+        return conn
+
+    def _get_https_connection(self):
+        conn = presto.dbapi.connect(
+            host=self.host,
+            port=self.port,
+            user=self.username,
+            http_scheme='https',
+            auth=presto.auth.BasicAuthentication(self.username, self.password),
+        )
+        return conn
+
+    def get_connection(self):
+        if self.conn:
+            return self.conn
+
+        if self.https:
+            self.conn = self._get_https_connection()
+            return self.conn
+        else:
+            self.conn = self._get_http_connection()
+            return self.conn
+
+    def get_cursor(self):
+        if not self.conn:
+            self.get_connection()
+        return self.conn.cursor()
+
+    def select(self, query):
+        cur = self.get_connection().get_cursor()
+        rs = cur.execute(query)
+        return rs
+
+    def transaction(self, query):
+        conn = self.get_connection()
+        cur = conn.get_cursor()
+        cur.execute(query)
+        conn.commit()
+
+
+class PrestoETLClient(PrestoClient):
+    def __init__(self, queries_dict, username, host, password=None, port=8080, https=False):
+        super().__init__(username, password, host, port, https)
+        self.queries_dict = queries_dict
+
+    def _template_query(self, file_path, context={}):
+        lines = '\n'.join(open(file_path, 'r').readlines())
+        template = Template(lines)
+        return template.render(**context)
+
+    def run(self):
+        for query, parameters in self.queries:
+            templated_query = self._template_query(query, parameters or {})
+            self.transaction(templated_query)
