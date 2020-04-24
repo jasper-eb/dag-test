4504dea9278ee70b87256f8354d275372d710485
jasper-eb <jasper@eventbrite.com>
2020-04-24 14:50:44 -0500

    Bugfix

--- library/etl/presto.py
+++ library/etl/presto.py
@@ -69,6 +69,6 @@ class PrestoETLClient(PrestoClient):
         return template.render(**context)
 
     def run(self):
-        for query, parameters in self.queries_dict:
+        for query, parameters in self.queries_dict.items():
             templated_query = self._template_query(query, parameters or {})
             self.transaction(templated_query)
