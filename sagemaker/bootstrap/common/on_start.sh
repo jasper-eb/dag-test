708cf751f8f51aef92372f093a532e7083168c4a
jasper-eb <jasper@eventbrite.com>
2020-06-23 15:30:10 -0500

    More bootstrapping, miniconda

--- sagemaker/bootstrap/common/on_start.sh
+++ sagemaker/bootstrap/common/on_start.sh
@@ -2,19 +2,24 @@
 
 set -e
 
+GITHUB_USER=jasper-eb
+GITHUB_TOKEN=$(aws secretsmanager get-secret-value --secret-id jasper | jq '.SecretString' | sed 's/\"{/{/g; s/}\"/}/g; s/\\//g' | jq '.github' | sed 's/\"//g')
+
 BOOTSTRAP_STAGING_DIR=/tmp/sagemaker_bootstrapping
-BOOTSTRAP_REPO_NAME=dag-test.git
-BOOTSTRAP_REPO=git@github.com:jasper-eb/$BOOTSTRAP_REPO_NAME
-BOOTSTRAP_REPO_RESOURCES_DIR=/sagemaker/bootstrap
+BOOTSTRAP_REPO_NAME=dag-test
+BOOTSTRAP_REPO=https://github.com/jasper-eb/$BOOTSTRAP_REPO_NAME.git
+BOOTSTRAP_REPO_RESOURCES_DIR=sagemaker/bootstrap
 
 # Get the owner of this notebook instance
 NOTEBOOK_ARN=$(jq '.ResourceArn' /opt/ml/metadata/resource-metadata.json --raw-output)
-USER_TAG=$(aws sagemaker list-tags --resource-arn $NOTEBOOK_ARN | jq '.Tags | .[] | select(.Key == "user") | .Value')
-USER=${USER_TAG//\"/}
+USER=$(aws sagemaker list-tags --resource-arn $NOTEBOOK_ARN | jq '.Tags | .[] | select(.Key == "user") | .Value' | sed 's/\"//g')
+
+yum install expect -y
 
 mkdir $BOOTSTRAP_STAGING_DIR
 cd $BOOTSTRAP_STAGING_DIR
-git clone $BOOTSTRAP_REPO
+
+echo "$GITHUB_USER\n$GITHUB_TOKEN" | git clone $BOOTSTRAP_REPO
 cd $BOOTSTRAP_REPO_NAME/$BOOTSTRAP_REPO_RESOURCES_DIR/$USER
 
 for BOOTSRAP_SCRIPT in *.sh; do
--- sagemaker/bootstrap/jasper/main.sh
+++ sagemaker/bootstrap/jasper/main.sh
@@ -2,4 +2,22 @@
 
 set -e
 
-yes | conda create --name main scipy tensorflow pandas presto-client
+KERNEL_DIR=second
+KERNEL_NAME="Second Kernel"
+
+WORKING_DIR="$(pwd)/$KERNEL_DIR"
+mkdir -p $WORKING_DIR
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$WORKING_DIR/miniconda.sh"
+bash $WORKING_DIR/miniconda.sh" -b -u -p "$WORKING_DIR/miniconda
+
+source $WORKING_DIR/miniconda/bin/activate
+pip install presto-client pytorch
+
+# Register kernel
+pip install ipykernel
+python -m ipykernel install --user --name $KERNEL_DIR --display-name "$KERNEL_NAME"
+
+# Cleanup
+source $WORKING_DIR/miniconda/bin/deactivate
+rm -rf $WORKING_DIR/miniconda.sh
